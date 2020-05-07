64ad8d26fccc0e5507b8272e5927a03b8e780b94
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-05-07 20:44:22 -0300

    login page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPage } from './pages/login/login.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage
   ],
   imports: [
     BrowserModule,
